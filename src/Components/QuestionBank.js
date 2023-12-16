3e62d2a8354fa509af4a244a533b5a39cade81ae
programmerviswanath <128244515+programmerviswanath@users.noreply.github.com>
2023-12-16 23:04:25 +0530

    Add files via upload

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What does the abbreviation HTML stand for?",
+		options: ["HyperText Markup Language", "HighText Markup Language", "HyperText Markdown Language", "None of the above"],
+		answer: "HyperText Markup Language",
+	},
+	{
+		id: 2,
+		question: "How many sizes of headers are available in HTML by default?",
+		options: ["5", "1", "3", "6"],
+		answer: "6",
+	},
+	{
+		id: 3,
+		question: "Which attribute is used to provide a unique name to an HTML element?",
+		options: ["id", "class", "type", "None of the above"],
+		answer: "id"
+	},
+	{
+		id: 4,
+		question: "Colors are defined in HTML using?",
+		options: ["RGB values", "HEX values", "RGBA values", "All of the above"],
+		answer: "All of the above"
+	},
+	{
+		id: 5,
+		question: "What tag is used to render an image on a webpage?",
+		options: ["img", "src", "image", "None of the above"],
+		answer: "img"
+	},
+]
+
+export default qBank;
